a2f761d92228b9d5794482c37aa33b6161a0c97c
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-14 00:30:10 +0300

    added header component and hoc layout

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './hoc/Layout';
+
+const Routes = () => {
+  return (
+    <Layout>
+      second line
+    </Layout>
+  );
+};
+
+export default Routes;
---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import  { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: '#98c5e9',
+          baxShadoe: 'none',
+          padding: '10px 0',
+          borderBottom: '2px solid #00285e'
+        }}
+      >
+        <Toolbar style={{ display: 'flex' }}>
+          <div style={{ flexGrow: 1 }}>
+            <div className="header_logo">
+              LOGO
+            </div>
+          </div>
+          <Link to="/team">
+            <Button color="inherit">The team</Button>
+          </Link>
+          <Link to="/matches">
+            <Button color="inherit">Matches</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
