ed19df63639a19df802aff95746d5f4e4252db6f
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-17 01:28:45 +0300

    updated Enroll component added form fields constructor

--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import FormField from '../../ui/FormFields';
+class Enroll extends Component {
+  state = {
+    formError: false,
+    formSuccess: '',
+    formData: {
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'email_input',
+          type: 'email',
+          placeholder: 'Enter your email'
+        },
+        validation: {
+          required: true,
+          email: true,
+        },
+        valid: false,
+        validationMessage: ''
+      }
+    }
+  }
 
-const Enroll = () => {
-  return (
-    <div>
-      enroll
-    </div>
-  );
-};
+  onSubmitForm = (event) => {
+
+  }
+
+  render() {
+    return (
+      <Fade>
+        <div className="enroll_wrapper">
+          <form onSubmit={this.onSubmitForm}>
+            <div className="enroll_title">
+              Enter your email
+            </div>
+            <div className="enroll_input">
+              <FormField
+                id={'email'}
+                formData={this.state.formData.email}
+              />
+            </div>
+          </form>
+        </div>
+      </Fade>
+    );
+  }
+}
 
 export default Enroll;
