05dfbcf96a1fc1af8120abaa556fc0a79f0a124b
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-18 21:23:49 +0300

    created private route

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Layout from './hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './components/auth-routes/PrivateRoute';
 import Home from './components/home';
 import SignIn from './components/signin';
 import Dashboard from './components/admin/Dashboard';
 
-const Routes = () => {
+const Routes = props => {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+        <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
         <Route exact component={SignIn} path="/signin" />
         <Route exact component={Home} path="/" />
       </Switch>
---
+++ src/components/auth-routes/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({
+  user,
+  component: Component,
+  ...rest
+}) => {
+  return <Route {...rest} component={(props) => (
+    user ?
+        <Component {...props} user={user} />
+      :
+      <Redirect to="/signin" />
+  )}/>
+};
+
+export default PrivateRoute;
