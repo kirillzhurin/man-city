6ae9ca3e3697ccf2e8dd5201cce83d638500a3f5
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-21 00:01:45 +0300

    included storage of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const config = {
   apiKey: "AIzaSyC3QOJdAdSFv4D9ew5AbnXYDJi6dqCFKOE",
   authDomain: "man-city-6fe6b.firebaseapp.com",
   databaseURL: "https://man-city-6fe6b.firebaseio.com",
   projectId: "man-city-6fe6b",
-  storageBucket: "",
+  storageBucket: "man-city-6fe6b.appspot.com",
   messagingSenderId: "482626697923",
   appId: "1:482626697923:web:d60051ae1357f7c0350076"
 };
