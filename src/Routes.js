86bf5da61d6bbe880e690ad8294232547d989ce8
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-21 21:18:03 +0300

    added page 404 - not found

--- src/Routes.js
+++ src/Routes.js
@@ -9,11 +9,14 @@ import Home from './components/home';
 import Team from './components/team';
 import Matches from './components/matches';
 import SignIn from './components/signin';
+import NotFound from './components/ui/NotFound';
+
 import Dashboard from './components/admin/Dashboard';
 import AdminMatches from './components/admin/matches';
 import AddEditMatch from './components/admin/matches/AddEditMatch';
 import AdminPlayers from './components/admin/players';
 import AddEditPlayer from './components/admin/players/AddEditPlayer';
+
 
 const Routes = props => {
   return (
@@ -30,6 +33,7 @@ const Routes = props => {
         <PublicRoute  {...props} restricted={false} exact component={Team} path="/team" />
         <PublicRoute  {...props} restricted={false} exact component={Matches} path="/matches" />
         <PublicRoute  {...props} restricted={false} exact component={Home} path="/" />
+        <PublicRoute  {...props} restricted={false} component={NotFound} />
       </Switch>
     </Layout>
   );
